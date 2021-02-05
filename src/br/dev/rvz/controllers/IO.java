ddf72ec050feb7211b6dd86a1f252f6454e8a60c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-05 10:15:12 -0300

    documentado a class IO

--- src/br/dev/rvz/controllers/IO.java
+++ src/br/dev/rvz/controllers/IO.java
@@ -3,10 +3,21 @@ package br.dev.rvz.controllers;
 import java.util.Scanner;
 
 public class IO {
+
+    /**
+     * proxy do scanner para leitura de daods no console/CLI
+     * @return Scanner - Retorna um novo objeto de Scanner
+     * @see Scanner
+     * */
     public static Scanner input() {
         return new Scanner(System.in);
     }
 
+    /**
+     * imprimi na tela do console o texto passsado
+     * @param String - texto a ser imprimido na tela
+     *
+     * */
     public static void out(String text) {
         System.out.println(text);
     }
