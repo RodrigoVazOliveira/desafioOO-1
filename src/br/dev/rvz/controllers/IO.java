f04ae33a1459aff06889922d3c12afbcce359dc2
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-05 10:12:22 -0300

    implementado proxy entrada e saide de dados

--- src/br/dev/rvz/controllers/IO.java
+++ src/br/dev/rvz/controllers/IO.java
@@ -1,5 +1,13 @@
 package br.dev.rvz.controllers;
 
-public class IO {
+import java.util.Scanner;
 
+public class IO {
+    public static Scanner input() {
+        return new Scanner(System.in);
+    }
+
+    public static void out(String text) {
+        System.out.println(text);
+    }
 }
