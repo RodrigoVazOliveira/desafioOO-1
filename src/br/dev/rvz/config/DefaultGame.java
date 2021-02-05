0ef76e5ecc889d6150c93b374b904365c974c40e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-05 11:10:23 -0300

    feito o serviço de novos jogos

--- src/br/dev/rvz/config/DefaultGame.java
+++ src/br/dev/rvz/config/DefaultGame.java
@@ -5,8 +5,7 @@ import br.dev.rvz.services.GenerateNumber;
 
 public class DefaultGame {
     public Game start() {
-        Game game = new Game(10, 11, 9);
-        game.setNumberRandom(GenerateNumber.next(20));
+        Game game = new Game(10, 5, GenerateNumber.next(20));
         return game;
     }
 }
--- src/br/dev/rvz/services/PlayerService.java
+++ src/br/dev/rvz/services/PlayerService.java
@@ -1,5 +1,6 @@
 package br.dev.rvz.services;
 
+import br.dev.rvz.controllers.IO;
 import br.dev.rvz.domain.Game;
 import br.dev.rvz.domain.Player;
 
@@ -13,15 +14,30 @@ public class PlayerService {
         this.game = game;
     }
 
-    public void startGaming(Integer number) {
 
+    public void startGaming() {
+        int pointsPlayer = player.getPoints();
+        pointsPlayer += printResult(player.getNumber());
+        player.setPoints(pointsPlayer);
     }
 
     private Integer validationAsset(Integer number) {
         if (number == game.getNumberRandom()) {
             return game.getPoint();
-        } else if (number == || number == game.getMinPoint()) {
-            return
+        } else if (number == game.getMinNumberRandom() || number == game.getMaxNumberRandom()) {
+            return game.getMinPoint();
+        } else {
+            return 0;
+        }
+    }
+
+    private Integer printResult(Integer number) {
+        int point = validationAsset(number);
+        if (point > 0) {
+            IO.out("Você acertou! Parabéns, você ganhou mais " + point);
+            return point;
+        } else {
+            IO.out("Você perdeu! Lamento.");
         }
     }
 
